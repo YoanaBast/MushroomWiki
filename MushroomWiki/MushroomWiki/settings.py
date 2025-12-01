0ed20ee1b5f5b2887cdb448174f8743706ad299d
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-12-01 20:42:31 +0200

    allowing host

--- MushroomWiki/MushroomWiki/settings.py
+++ MushroomWiki/MushroomWiki/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mushroomwiki.onrender.com']
 
 
 # Application definition
