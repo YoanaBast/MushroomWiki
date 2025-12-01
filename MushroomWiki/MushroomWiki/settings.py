c7c7fae405672d95bf79bebcba23fe558bae4fe9
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-12-01 20:53:24 +0200

    adding static root, whitenoise

--- MushroomWiki/MushroomWiki/settings.py
+++ MushroomWiki/MushroomWiki/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'MushroomWiki.urls'
@@ -141,6 +143,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
